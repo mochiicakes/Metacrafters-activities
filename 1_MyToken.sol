1d5e1ca01a5d2513955c89927bede52d7aa28e4e
mochiicakes <144116370+mochiicakes@users.noreply.github.com>
2023-09-26 22:54:02 +0800

    Update 1_MyToken.sol

--- 1_MyToken.sol
+++ 1_MyToken.sol
@@ -7,53 +7,46 @@ pragma solidity >=0.8.2 <0.9.0;
  * @dev Store & retrieve value in a variable
  * @custom:dev-run-script ./scripts/deploy_with_ethers.ts
  */
+
+ /*
+       REQUIREMENTS
+    1. Your contract will have public variables that store the details about your coin (Token Name, Token Abbrv., Total Supply)
+    2. Your contract will have a mapping of addresses to balances (address => uint)
+    3. You will have a mint function that takes two parameters: an address and a value. 
+       The function then increases the total supply by that number and increases the balance 
+       of the “sender” address by that amount
+    4. Your contract will have a burn function, which works the opposite of the mint function, as it will destroy tokens. 
+       It will take an address and value just like the mint functions. It will then deduct the value from the total supply 
+       and from the balance of the “sender”.
+    5. Lastly, your burn function should have conditionals to make sure the balance of "sender" is greater than or equal 
+       to the amount that is supposed to be burned.
+*/
+
 contract MyToken {
-    string public name;
-    string public symbol;
-    uint256 public totalSupply;
+    string public tName;
+    string public symb;
+    uint256 public tSupply;
 
     mapping(address => uint256) public balances;
 
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Mint(address indexed to, uint256 value);
-    event Burn(address indexed from, uint256 value);
-
-    address public owner;
-
-    constructor(string memory _name, string memory _symbol, uint256 _initialSupply) {
-        name = _name;
-        symbol = _symbol;
-        totalSupply = _initialSupply;
-        balances[msg.sender] = _initialSupply;
-        owner = msg.sender;
-    }
-
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Only the owner can call this function");
-        _;
+    constructor(string memory _tName, string memory _symb, uint256 _initSupply) 
+    {
+        tName = _tName;
+        symb = _symb;
+        tSupply = _initSupply;
+        balances[msg.sender] = _initSupply;
     }
 
     receive() external payable {
     }
 
-    function mint(address _to, uint256 _value) public onlyOwner {
-        require(_to != address(0), "Invalid address");
-        require(_value > 0, "Value must be greater than zero");
-
-        totalSupply += _value;
+    function mint(address _to, uint256 _value) public {
+        tSupply += _value;
         balances[_to] += _value;
-        emit Mint(_to, _value);
-        emit Transfer(address(0), _to, _value);
     }
 
-    function burn(address _from, uint256 _value) public onlyOwner {
-        require(_from != address(0), "Invalid address");
-        require(balances[_from] >= _value, "Insufficient balance");
-        require(_value > 0, "Value must be greater than zero");
-
-        totalSupply -= _value;
+    function burn(address _from, uint256 _value) public {
+        tSupply -= _value;
         balances[_from] -= _value;
-        emit Burn(_from, _value);
-        emit Transfer(_from, address(0), _value);
     }
 }
